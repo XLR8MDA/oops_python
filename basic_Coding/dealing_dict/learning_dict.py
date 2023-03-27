144e1f9be52582cb3486e775b19b39db01f5979b
XLR8090 <xlr8090@gmail.com>
2023-03-27 22:44:15 +0530

    new things added

---
+++ basic_Coding/dealing_dict/learning_dict.py
@@ -0,0 +1,16 @@
+'''Dictionaries are a data structure that allows you to store data in key-value pairs.  
+Dictionaries are mutable, meaning that you can change the data in a dictionary after it has been created.
+
+These are used to solve problems like finding the frequency of characters in a string, 
+finding the frequency of words in a paragraph '''
+
+#declaring a dictionary
+my_dict={}                                  #empty dictionary
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+
+for key in my_dict.keys():
+    print(key)
+
+
--- practice.py
+++ practice.py
@@ -1,1 +1,16 @@
-print(21)
+my_dict = {'a': 5, 'b': 3, 'c': 8, 'd': 2}
+
+#print key and values in reverse order
+for key, value in (my_dict.items()):
+    print(key, value)
+
+
+#my_dict functions
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+
+#sort by key
+for key in sorted(my_dict.keys()):
+    print(key, my_dict[key])
+    
